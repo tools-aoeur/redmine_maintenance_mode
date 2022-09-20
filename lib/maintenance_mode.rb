f443fcfd63fef8c3ceed46f640982028532b83c2
Jaap de Haan <jaap.dehaan@alpsalpine.com>
2022-09-20 10:48:49 +0200

    fix: Redmine 5 support

--- lib/maintenance_mode.rb
+++ lib/maintenance_mode.rb
@@ -1,29 +1,24 @@
 module MaintenanceMode
-  require 'maintenance_mode_functions'
-  
+  require_relative 'maintenance_mode_functions'
+
   def self.included(base)
     base.class_eval do
       unloadable
-      
+
       # show maintenance message for all "normal" users (except admins)
       def show_maintenance_mode_page
-        
         # read plugin settings
         settings = MaintenanceModeFunctions.get_maintenance_plugin_settings
-                
-        # only activate maintenance message if maintenance mode is activated or if we're in the middle of a scheduled maintenance 
-        if settings[:maintenance_active] || MaintenanceModeFunctions.is_now_scheduled_maintenance
-          # and only activate it for non-admin users (or sudoers if 'redmine_sudo' plugin is installed)
-          unless User.current.admin? || (Redmine::Plugin.installed?("redmine_sudo") && User.current.sudoer?)
-            logout_user if User.current.logged?
-            require_login unless params[:controller] == "account" && params[:action] == "login"
-            return false
-          end
+
+        # only activate maintenance message if maintenance mode is activated or if we're in the middle of a scheduled maintenance
+        if (settings[:maintenance_active] || MaintenanceModeFunctions.is_now_scheduled_maintenance) && !(User.current.admin? || (Redmine::Plugin.installed?('redmine_sudo') && User.current.sudoer?))
+          logout_user if User.current.logged?
+          require_login unless params[:controller] == 'account' && params[:action] == 'login'
+          false
         end
       end
-      
+
       before_action(:show_maintenance_mode_page)
-      
     end
   end
 end
