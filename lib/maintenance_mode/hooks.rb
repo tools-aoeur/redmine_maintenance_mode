1396ca7d5243723f820aa52580682567a016a05c
Jaap de Haan <261428+jdehaan@users.noreply.github.com>
2026-01-09 14:18:57 +0100

    fix: File not found admin.css

--- lib/maintenance_mode/hooks.rb
+++ lib/maintenance_mode/hooks.rb
@@ -7,7 +7,7 @@ module MaintenanceMode
 
     # add css and javascript if we're in the middle of a maintenance
     def view_layouts_base_html_head(_context = {})
-      tags = stylesheet_link_tag('admin', plugin: 'redmine_maintenance_mode')
+      tags = ''
       tags += stylesheet_link_tag 'maintenance_mode', plugin: 'redmine_maintenance_mode' if Functions.notification?
       tags
     end
